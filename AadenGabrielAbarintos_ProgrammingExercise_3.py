f4660af95a12570efd70498bad0eb79d06c9a36c
aaden <adetrixwithasix@gmail.com>
2025-09-22 17:54:45 -0400

    Initial commit

---
+++ AadenGabrielAbarintos_ProgrammingExercise_3.py
@@ -0,0 +1,5 @@
+# AadenGabrielAbarintos_ProgrammingExercise_2.py
+# This program asks the user for a list of their monthly
+# expenses as well as the type of expense, and the total amount
+# of the expense. The program then analyzes the list and displays
+# the total expense, highest expense, and the lowest expense.
