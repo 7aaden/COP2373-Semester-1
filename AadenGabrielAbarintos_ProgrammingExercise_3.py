86bdc38620a09b141823f482c313685be73f919a
aaden <adetrixwithasix@gmail.com>
2025-09-26 19:14:53 -0400

    Defined the process in which the user is asked their expense type and the amount.

--- AadenGabrielAbarintos_ProgrammingExercise_3.py
+++ AadenGabrielAbarintos_ProgrammingExercise_3.py
@@ -17,13 +17,52 @@ def main():
     print("Please list your monthly expenses below.")
 
     # Create a While loop to ask the user for their
-    # until they press enter.
+    # until they type "done".
+    while True:
 
+        # Ask the user for one of their expenses.
+        expense_type = input("Enter the type of expense here "
+        "(type done when finished listing your expenses): "
+        )
+
+        # If the user inputs done, end the while loop.
+        if expense_type.lower() == "done":
+            break
+        
+        # Ask the user for the amount of their specific expense.
+        try:
+            expense_amount = float(input(
+                f"Enter the amount for {expense_type} here: $"
+            ))
+
+            # Add the data to the dictionary
+            expenses.append(expense_type, expense_amount)
+
+        # Raise a value error if they do not input a number.
+        except ValueError:
+            print("Invalid input. Please input a number.")
+    
+    # If the user did not enter any expenses then return.
+    if not expenses:
+        print("No expenses entered.")
+        return
+    
+# Calculate the total expense
+
+# Calculate the highest expense
+
+# Calculate the lowest expense
 
     # Display the results
+    print ("Expense Report: ")
+    print()
+    print("Total Expenses: ")
+    print("Highest Expense: ")
+    print("Lowest Expense: ")
 
-    
-    
+
+# Run the main function
+main()
 
 
 
