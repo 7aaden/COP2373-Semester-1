471ebe5034696539b44596ebc9f2cc010e380e4e
aaden <adetrixwithasix@gmail.com>
2025-09-26 18:32:47 -0400

    Imported functools, defined main function, created an empty dictionary

--- AadenGabrielAbarintos_ProgrammingExercise_3.py
+++ AadenGabrielAbarintos_ProgrammingExercise_3.py
@@ -3,3 +3,29 @@
 # expenses as well as the type of expense, and the total amount
 # of the expense. The program then analyzes the list and displays
 # the total expense, highest expense, and the lowest expense.
+
+# Import the functools module
+import functools
+
+# Define the main function.
+def main():
+
+    # Create an empty dictionary of expenses
+    expenses = []
+
+    # Ask the user for their list of their monthly expenses.
+    print("Please list your monthly expenses below.")
+
+    # Create a While loop to ask the user for their
+    # until they press enter.
+
+
+    # Display the results
+
+    
+    
+
+
+
+
+
