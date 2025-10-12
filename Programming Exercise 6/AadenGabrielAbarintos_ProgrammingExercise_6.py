375b6b12aa608b3f9209701feaaae0d9b91216c0
aaden <adetrixwithasix@gmail.com>
2025-10-12 16:25:57 -0400

    Created the validation functions and the main function.

--- Programming Exercise 6/AadenGabrielAbarintos_ProgrammingExercise_6.py
+++ Programming Exercise 6/AadenGabrielAbarintos_ProgrammingExercise_6.py
@@ -1,1 +1,45 @@
 # AadenGabrielAbarintos_ProgrammingExercise_6
+# This program writes an application that validates data,
+# specifically phone numbers, social security numbers,
+# and zip codes using regular expressions.
+
+# Import the regular expression module
+import re
+
+# Validate the phone number
+def validate_phone(phone):
+
+    phone_pattern = re.compile()
+
+
+# Validate the Social Security Number
+def validate_ssn(ssn):
+
+    ssn_pattern = re.compile()
+
+
+# Validate the ZIP code
+def validate_zip(zip_code):
+
+    zip_code_pattern = re.compile()
+
+
+# Initialize the main function
+def main():
+
+    # Ask the user for their credentials
+    print("Credential Validation Test: ")
+    phone = input("Enter your phone number: ")
+    ssn = input ("Enter your Social Security Number: ")
+    zip_code = input("Enter your ZIP code: ")
+
+    # Display the data
+    print("Validity Test Results: ")
+    print()
+    print(f"Phone Number: {validate_phone(phone)}")
+    print(f"SSN: {validate_ssn(ssn)}")
+    print(f"ZIP Code: {validate_zip(zip_code)}")
+
+
+# Run the main function
+main()
