5d0d802b1320937d63b8657104546d62b0f15779
aaden <adetrixwithasix@gmail.com>
2025-10-13 16:33:14 -0400

    Inital commit.

---
+++ Programming Exercise 7/AadenGabrielAbarintos_ProgrammingExercise_7.py
@@ -0,0 +1,14 @@
+# AadenGabrielAbarintos_ProgrammingExercise_8.py
+# This program allows the user to enter a paragraph,
+# including sentences that begin with numbers.
+# The program then displays each individual sentence and
+# counts how many sentences are in the paragraph.
+
+
+# Define the main function
+def main():
+    pass
+
+
+# Run the main function
+main()
