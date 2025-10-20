a8a86fb8ff432d8696296d10b4ae21015321a907
aaden <adetrixwithasix@gmail.com>
2025-10-19 21:41:02 -0400

    Did not commit a while ago. Just a placeholder so that I could work on it later.

--- Programming Exercise 7/AadenGabrielAbarintos_ProgrammingExercise_7.py
+++ Programming Exercise 7/AadenGabrielAbarintos_ProgrammingExercise_7.py
@@ -4,11 +4,18 @@
 # The program then displays each individual sentence and
 # counts how many sentences are in the paragraph.
 
+# Import the regular expression module
+import re
+
 
 # Define the main function
 def main():
     pass
 
+
+# Define the function
+def placeholder(x):
+    pass
 
 # Run the main function
 main()
