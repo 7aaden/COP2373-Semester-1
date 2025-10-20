c756cb4fa376750199e1105cc11e91aa20001252
aaden <adetrixwithasix@gmail.com>
2025-10-19 22:36:22 -0400

    Final commit.

--- Programming Exercise 7/AadenGabrielAbarintos_ProgrammingExercise_7.py
+++ Programming Exercise 7/AadenGabrielAbarintos_ProgrammingExercise_7.py
@@ -10,12 +10,32 @@ import re
 
 # Define the main function
 def main():
-    pass
+    
+    # Ask the user to input their paragraph
+    paragraph = input("Enter a paragraph: ")
+
+    # Split the paragraph
+    sentences = split_sentences(paragraph)
+
+    # Display the data
+    print("Individual Sentences:")
+    print()
+
+    for n, s in enumerate(sentences, 1):
+        print(f"{n}. {s}")
+    
+    # Display the total number sentences in the paragraph
+    print(f"\nTotal number of sentences: {len(sentences)}")
 
 
-# Define the function
-def placeholder(x):
-    pass
+# Define the split_sentences function
+def split_sentences(paragraph):
+
+    # Create the pattern to split the paragraph.
+    pattern = r'(?<=[.!?])\s+(?=[A-Z0-9])'
+
+    # Return the paragraph as a list of split sentences.
+    return re.split(pattern, paragraph)
 
 # Run the main function
 main()
