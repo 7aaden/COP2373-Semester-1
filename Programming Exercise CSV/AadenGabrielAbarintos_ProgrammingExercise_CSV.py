13cbe434d850a8b0460b799dacf4e5d8ce465df0
aaden <adetrixwithasix@gmail.com>
2025-10-26 19:26:05 -0400

    Initial commit.

---
+++ Programming Exercise CSV/AadenGabrielAbarintos_ProgrammingExercise_CSV.py
@@ -0,0 +1,20 @@
+# AadenGabrielAbarintos_ProgrammingExercise_CSV.py
+# This program allows the user to input information of their students.
+# The program allows the user to input their students' first names,
+# last names, and three exam grades. The names are inputted as strings
+# and the exam grades are inputted as integers.
+# The program then reads the file and then displays the data
+# in tabular format.
+
+# Import the CSV module
+import csv
+
+
+# Define the write_grades function
+def write_grades():
+    pass
+
+
+# Define the read_grades function
+def read_grades():
+    pass
