235986fa5a1c1161f1dd5e49f1c911f859056f6f
aaden <adetrixwithasix@gmail.com>
2025-10-30 18:18:36 -0400

    Initial commit

---
+++ Programming Exercise 9/AadenGabrielAbarintos_ProgrammingExercise_9.py
@@ -0,0 +1,8 @@
+# AadenGabrielAbarintos_ProgrammingExercise_9.py
+# This program creates a class that contains the name,
+# account number, amount, and interest rate of an account.
+# The class then supports methods for adjustng the interst rate,
+# for withdrawing, depositing, and for giving a balance.
+
+class BankAcct:
+    pass
