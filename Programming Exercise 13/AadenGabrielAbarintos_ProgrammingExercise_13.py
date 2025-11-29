86133258d9005d9323e6d5063d50f799e967dabf
aaden <adetrixwithasix@gmail.com>
2025-11-28 23:29:57 -0500

    Initial commit

---
+++ Programming Exercise 13/AadenGabrielAbarintos_ProgrammingExercise_13.py
@@ -0,0 +1,44 @@
+# AadenGabrielAbarintos_ProgrammingExercise_13.py
+# This program creates a database called population_AA
+# then creates a table within that database with the fields
+# city, year, and population. The data is composed of
+# ten cities within the state of Florida from the year 2023. 
+# The program then simulates population growth and decline 
+# for the next 20 years. Then the program shows the 
+# population growth for a city. The user then is prompted
+# to select a city to display the data.
+
+
+# Import the SQLite module.
+import sqlite3
+
+
+# Import the random module.
+import random
+
+
+# Import the matplotlib module.
+import matplotlib.pyplot as plt
+
+
+# Define the create_db function.
+def create_db():
+    pass
+
+
+# Define the simulate function.
+def simulate():
+    pass
+
+# Define the plot_population function.
+def plot_city_population():
+    pass
+
+
+# Define the main function.
+def main():
+    pass
+
+
+# Run the main function.
+main()
